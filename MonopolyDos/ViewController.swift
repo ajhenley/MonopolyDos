9efcb3a337b070dad2bc6d13460feedac1294e94
AJ Henley <ajhenley@gmail.com>
2018-06-19 14:31:02 -0400

    First Commit

--- MonopolyDos/ViewController.swift
+++ MonopolyDos/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImage: UIImageView!
+    @IBOutlet weak var rightImage: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        leftImage.image = UIImage(named: "dice2.jpg")
+        myLabel.textColor = UIColor.red
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
